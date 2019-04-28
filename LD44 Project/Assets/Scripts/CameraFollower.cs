fb3c9a35c9ffb56eb0b3e215fe5cff465a9fd369
fr4ct1ons <42714282+fr4ct1ons@users.noreply.github.com>
2019-04-28 00:51:01 -0300

    Added a Player-Follower and a Camera-Follower
    Player-Follower makes enemies follow the player around. Camera-Follower does the same, except in camera terms.

---
+++ LD44 Project/Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    GameObject player;
+    Vector3 bufferVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bufferVector.Set(player.transform.position.x, player.transform.position.y, -10);
+        transform.position = bufferVector;
+    }
+}
---
+++ LD44 Project/Assets/Scripts/Enemies/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, movementSpeed * Time.deltaTime);
+    }
+}
