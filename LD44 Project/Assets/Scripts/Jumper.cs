24d85d92f0aada82c89c22486c1ea19b76675f03
fr4ct1ons <42714282+fr4ct1ons@users.noreply.github.com>
2019-06-15 11:59:48 -0300

    Added Jumper enemy

---
+++ LD44 Project/Assets/Scripts/Jumper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    [SerializeField] GameObject myRoom;
+    [SerializeField] float speed;
+    [SerializeField] float timeBetweenJumps;
+
+    Vector2 bottomL, topR;
+    Vector3 bufferVector;
+    bool canJump = true, dontCount = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        topR = myRoom.GetComponent<Room>().GetTopR();
+        bottomL = myRoom.GetComponent<Room>().GetBottomL();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            bufferVector.Set(UnityEngine.Random.Range(bottomL.x, topR.x), UnityEngine.Random.Range(bottomL.y, topR.y), 0);
+            Debug.Log(bufferVector);
+        }
+        else if (transform.position == bufferVector && dontCount)
+        {
+            StartCoroutine(Wait());
+        }
+        else
+            transform.position = Vector3.MoveTowards(transform.position, bufferVector, speed * Time.deltaTime);
+    }
+
+    
+    private IEnumerator Wait()
+    {
+        dontCount = false;
+        yield return new WaitForSeconds(timeBetweenJumps);
+        dontCount = true;
+        canJump = true;
+    }
+    
+}
